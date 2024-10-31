e8d96ec42156e2f2462d24077c18fbd00ecd5f75
Jesús Badenas <jesus.badenas@agilecontent.com>
2024-10-31 12:28:48 +0100

    Update dependencies

--- test/src/main/java/com/jesusbadenas/kotlin_clean_compose_project/test/CoroutinesTestRule.kt
+++ test/src/main/java/com/jesusbadenas/kotlin_clean_compose_project/test/CoroutinesTestRule.kt
@@ -2,25 +2,25 @@ package com.jesusbadenas.kotlin_clean_compose_project.test
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 @ExperimentalCoroutinesApi
-class CoroutinesTestRule(private val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()) :
-    TestWatcher(),
-    TestCoroutineScope by TestCoroutineScope(dispatcher) {
+class CoroutinesTestRule(
+        private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+) : TestWatcher() {
+
     override fun starting(description: Description?) {
         super.starting(description)
-        Dispatchers.setMain(dispatcher)
+        Dispatchers.setMain(testDispatcher)
     }
 
     override fun finished(description: Description?) {
         super.finished(description)
         Dispatchers.resetMain()
-        dispatcher.cleanupTestCoroutines()
     }
 }
