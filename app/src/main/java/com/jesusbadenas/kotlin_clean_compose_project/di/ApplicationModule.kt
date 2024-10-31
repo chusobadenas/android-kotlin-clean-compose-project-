c28d45bf3d440372b52643a71af49bd639403fb9
Jesús Badenas <jesus.badenas@agilecontent.com>
2024-10-31 12:30:56 +0100

    Update deprecated code

--- app/src/main/java/com/jesusbadenas/kotlin_clean_compose_project/di/ApplicationModule.kt
+++ app/src/main/java/com/jesusbadenas/kotlin_clean_compose_project/di/ApplicationModule.kt
@@ -8,7 +8,7 @@ import com.jesusbadenas.kotlin_clean_compose_project.viewmodel.MainViewModel
 import com.jesusbadenas.kotlin_clean_compose_project.viewmodel.UserDetailsViewModel
 import com.jesusbadenas.kotlin_clean_compose_project.viewmodel.UserListViewModel
 import org.koin.androidx.fragment.dsl.fragment
-import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
