cd18df387bcf11926f562d0c3c2d33c04a64097c
Jesús Badenas <jesus.badenas@agilecontent.com>
2024-12-24 11:59:05 +0100

    Update dependencies

--- domain/src/main/java/com/jesusbadenas/kotlin_clean_compose_project/domain/interactor/GetUser.kt
+++ domain/src/main/java/com/jesusbadenas/kotlin_clean_compose_project/domain/interactor/GetUser.kt
@@ -3,7 +3,9 @@ package com.jesusbadenas.kotlin_clean_compose_project.domain.interactor
 import com.jesusbadenas.kotlin_clean_compose_project.domain.model.User
 import com.jesusbadenas.kotlin_clean_compose_project.domain.repository.UserRepository
 
-class GetUser(private val userRepository: UserRepository) {
+class GetUser(
+    private val userRepository: UserRepository
+) {
 
     suspend operator fun invoke(userId: Int): User = userRepository.user(userId)
 }
