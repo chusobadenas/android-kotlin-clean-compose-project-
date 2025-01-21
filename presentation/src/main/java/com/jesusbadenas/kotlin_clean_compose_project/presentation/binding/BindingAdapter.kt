9d074f6206bec74427a4decedd4e293a47525b83
Jesús Badenas <chusobadenas@gmail.com>
2025-01-21 15:04:41 +0100

    Upgrade Coil with Compose

--- presentation/src/main/java/com/jesusbadenas/kotlin_clean_compose_project/presentation/binding/BindingAdapter.kt
+++ presentation/src/main/java/com/jesusbadenas/kotlin_clean_compose_project/presentation/binding/BindingAdapter.kt
@@ -4,16 +4,17 @@ import android.view.View
 import android.widget.ImageView
 import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
-import coil.load
+import coil3.load
+import coil3.request.crossfade
+import coil3.request.error
+import coil3.request.placeholder
 import com.jesusbadenas.kotlin_clean_compose_project.presentation.R
 
 @BindingAdapter("imageUrl")
-fun ImageView.setImageUrl(imageUrl: String?) {
-        this.load(imageUrl) {
-            crossfade(true)
-            error(R.color.error)
-            placeholder(R.color.error)
-        }
+fun ImageView.setImageUrl(imageUrl: String?) = this.load(imageUrl) {
+    crossfade(true)
+    error(R.color.error)
+    placeholder(R.color.error)
 }
 
 @BindingAdapter("isVisible")
